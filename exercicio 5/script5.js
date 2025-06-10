400628e367917d1fa2303630bf05d636765c9803
Nathan <142441493+Ceag1999@users.noreply.github.com>
2025-06-10 10:51:00 -0300

    Add files via upload

---
+++ exercicio 5/script5.js
@@ -0,0 +1,10 @@
+function calcular(){
+let num1 = Number(window.prompt("digte aqui o primeiro numero"));
+let num2 = Number(window.prompt("digite aqui o segundo numero"));
+
+if (num1 > num2){
+    aler(num1 + " é maior que " + num2)
+} else {
+    alert(num2 + " é maior que " + num1)
+  }
+}
