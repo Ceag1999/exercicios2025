2110481ab2d46c759093ea97f2b83cac100172a4
Nathan <142441493+Ceag1999@users.noreply.github.com>
2025-06-10 11:52:26 -0300

    Add files via upload

--- exercicio 4/script4.js
+++ exercicio 4/script4.js
@@ -1,6 +1,11 @@
 let inscritos = 0;
+let equipes = 0;
 
 function calcular(){
     inscritos +=1;
-    alert("número de inscritos: " + inscritos)
+    equipes = parseInt (inscritos / 3);
+    alert("número de inscritos: " + inscritos + ". equipes: " + equipes )
 }
+ if(equipes >= 4) {
+    alert("numero de incritos: " + inscritos + "numero de equipes: " + equipes)
+ }
