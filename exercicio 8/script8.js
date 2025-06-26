865b2aa37d183dd1aeb964e566080ce8ccb5871b
Nathan <142441493+Ceag1999@users.noreply.github.com>
2025-06-26 10:43:50 -0300

    Add files via upload

--- exercicio 8/script8.js
+++ exercicio 8/script8.js
@@ -1,1 +1,22 @@
 let mostraHTML = document.getElementById("mostrahtml");
+let numero = 0
+let num_adiv = 0
+
+function adiv(){
+    let frase
+
+    num_adiv = Number(window.prompt("digite um numero de 1 a 100"));
+
+    numero = parseInt(Math.random() * 101);
+
+    frase = mostraHTML.innerHTML += `<p> voce escolheu ${num_adiv}.</p>`
+
+    if(num_adiv > numero){
+        mostraHTML.innerHTML = `<p> voce escolheu ${num_adiv}. meu numero é MENOR </p>`
+    }else{
+        mostraHTML.innerHTML = `<p> voce escolheu ${num_adiv}. meu numero é MAIOR </p>`  
+    }
+    if (num_adiv <= numero){
+        mostraHTML.innerHTML = `<p> parabens voce ganhou! </p>`
+    }
+}
---
+++ exercicio 9/script9.js
@@ -0,0 +1,55 @@
+let mostraHTML = document.getElementById("mostraHTML");
+function estaçao(){
+    
+    let mes = prompt("digite um mes").toUpperCase()
+
+    console.log(mes);
+if(mes == "JANEIRO") {
+    mostraHTML.innerHTML = `<p> voce falou o ${mes}, estaçao do ano e verao <p/>`
+}  
+
+if(mes == "FEVEREIRO") {
+    mostraHTML.innerHTML = `<p> voce falou o ${mes}, estaçao do ano e verao <p/>`
+} 
+
+if(mes == "MARÇO") {
+    mostraHTML.innerHTML = `<p> voce falou o ${mes}, estaçao do ano e outono <p/>`
+}
+
+if(mes == "ABRIL") {
+    mostraHTML.innerHTML = `<p> voce falou o ${mes}, estaçao do ano e outono <p/>`
+}
+
+if(mes == "MAIO") {
+    mostraHTML.innerHTML = `<p> voce falou o ${mes}, estaçao do ano e outono<p/>`
+}
+
+if(mes == "JUNHO") {
+    mostraHTML.innerHTML = `<p> voce falou o ${mes}, estaçao do ano e inverno <p/>`
+}
+
+if(mes == "JULHO") {
+    mostraHTML.innerHTML = `<p> voce falou o ${mes}, estaçao do ano e inverno <p/>`
+}
+   
+if(mes == "AGOSTO") {
+        mostraHTML.innerHTML = `<p> voce falou o ${mes}, estaçao do ano e inverno <p/>`
+}
+
+if(mes == "SETEMBRO") {
+    mostraHTML.innerHTML = `<p> voce falou o ${mes}, estaçao do ano e primavera <p/>`
+}
+
+if(mes == "OUTUBRO") {
+    mostraHTML.innerHTML = `<p> voce falou o ${mes}, estaçao do ano e primavera <p/>`
+}
+
+if(mes == "NOVEMBRO") {
+    mostraHTML.innerHTML = `<p> voce falou o ${mes}, estaçao do ano e primaverac <p/>`
+}
+
+if(mes == "DEZEMBRO") {
+    mostraHTML.innerHTML = `<p> voce falou o ${mes}, estaçao do ano e verao <p/>`
+}
+
+}
